05d882e154545e5269e0e89711c5c9c5b09f6c8b
Marina Tripetska <marina.tripetskaya@gmail.com>
2022-07-08 10:41:11 +0200

    init: start proj

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(cors());
+app.use(express.json());
+app.use(logger(formatsLogger));
+
+app.use((_, res, next) => {
+  next({ status: 404, message: "Not found" });
+});
+
+app.use((err, _, res, next) => {
+  const { status = 500, message = "Internal Server Error" } = err;
+  res.status(status).json({ message });
+});
+
+module.exports = app;
