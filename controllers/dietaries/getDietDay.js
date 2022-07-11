e78419c2a6cccaba474cf1b28393089e941d2d29
Marina Tripetska <marina.tripetskaya@gmail.com>
2022-07-11 23:21:56 +0200

    feat: create and update dietary

--- controllers/dietaries/getDietDay.js
+++ controllers/dietaries/getDietDay.js
@@ -9,7 +9,12 @@ const getDietDay = async (req, res, next) => {
     const findDay = await Dietary.findOne({
       owner: _id,
       date: date,
-    }).populate("owner", "_id name email");
+    })
+      .populate("owner", "_id name email")
+      .populate({
+        path: "products.product",
+        select: "title calories",
+      });
 
     if (!findDay) {
       return createError(404, "Not found");
--- services/dietaryService/updateDietary.js
+++ services/dietaryService/updateDietary.js
@@ -1,20 +1,20 @@
 const { Dietary } = require("../../models");
 
 const updateDietary = async (_id, payload) => {
-  const { date, products } = payload;
+  const { date, product } = payload;
 
   return await Dietary.findOneAndUpdate(
     {
       owner: _id,
       date: date,
     },
-    { products },
+    { $push: { products: product } },
     { new: true }
   )
     .populate("owner", "name email")
     .populate({
       path: "products.product",
-      select: "title",
+      select: "title calories",
     });
 };
 
