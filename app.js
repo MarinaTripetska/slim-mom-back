929bdff7f447ca0bfb0a91aae34428d20e9747fb
Yaroslav Ilchenko <ilchenckoyaroslav@gmail.com>
2022-07-14 02:30:05 +0300

    initial swagger

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-const { userRouter, productsRouter, dietariesRouter, authRouter } = require("./routes/api");
+const {
+  userRouter,
+  productsRouter,
+  dietariesRouter,
+  authRouter,
+} = require("./routes/api");
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(cors());
 app.use(express.json());
 app.use(logger(formatsLogger));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/v1/users", authRouter, userRouter);
 app.use("/api/v1/products", productsRouter);
